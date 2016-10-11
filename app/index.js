6473cd3fddc4a2f27000ac8819d1f8de2f067bdc
Jordan Cotter <Jordan@rickkochoil.com>
2016-10-11 08:42:57 -0500

    added avatar

--- app/index.js
+++ app/index.js
@@ -1,17 +1,57 @@
 const 
-	React = require ('react')
+  React = require ('react')
 , ReactDOM = require ('react-dom')
-, HelloWorld = React.createClass ({
-		render: function () {
-			return (
-				<div> Hello World </div>
-      )
-		}
+, USER_DATA = {
+    name: 'Casey Cotter',
+    username: 'cacotter',
+    image: 'http://www.asapenergyinc.com/wp-content/uploads/2015/10/DSCF0014.jpg'
+  }
+, ProfilePic = React.createClass({
+    render: function () {
+      return <img src={this.props.imageUrl} style={{width: 100, height: 100}} />
+    }
   })
+, ProfileLink = React.createClass({
+    render: function () {
+      return (
+        <div>
+	  <a href={'https://www.github.com/' + this.props.username}>
+	    {this.props.username}
+	  </a>
+	</div> 
+      )
+    }
+  })
+, ProfileName = React.createClass({
+    render: function () {
+      return (
+        <div>{this.props.name}</div>
+      )
+    }
+  })
+, Avatar = React.createClass({
+    render: function () {
+      return (
+        <div>
+	  <ProfilePic imageUrl={this.props.user.image} />
+	  <ProfileName name={this.props.user.name} />
+	  <ProfileLink username ={this.props.user.username} />
+	</div>
+      )
+    }
+  })
+
+  /*FIRST
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+  */
   
-,	nil = null;
+, nil = null;
     
 ReactDOM.render (
-	<HelloWorld />
+  <Avatar user={USER_DATA} />
 , document.getElementById('app')
 )
