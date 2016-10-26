52dd6b59599a0be593ea3d94830b25c9ba17bc88
cotterjd <jrdn.cttr@gmail.com>
2016-10-26 08:55:00 -0500

    made player one and player two pages work

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,17 +1,52 @@
 const
   React = require('react')
 , transparentBg = require('../styles').transparentBg
+
 , PromptContainer = React.createClass({
+    getInitialState: function () {
+      return {
+        username: ''
+      }
+    },
+    contextTypes: {
+      router: React.props
+    },
+    onUpdateUser: function (e) {
+      this.setState({
+        username: e.target.value
+      })
+    },
+    onSubmitUser: function (e) {
+      e.preventDefault();
+      const username = this.state.username;
+      this.setState({
+        username: ''
+      });
+
+      if (this.props.routeParams.playerOne) {
+        this.context.router.push({
+          pathname: '/battle',
+	  query: {
+	    playerOne: this.props.routeParams.playerOne,
+	    playerTwo: this.state.username
+	  }
+	})
+      } else {
+        this.context.router.push ('/playerTwo/' + this.state.username)
+      }
+    },
     render: function(){
       return (
         <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
           <h1>{this.props.route.header}</h1>
 	  <div className="col-sm-12">
-	    <form>
+	    <form onSubmit={this.onSubmitUser}>
 	      <div className="form-group">
 	        <input
 		  className="form-control"
 		  placeholder="Github Username"
+		  onChange={this.onUpdateUser}
+		  value={this.state.username}
 		  type="text" />
 	      </div>
 	      <div className="form-group col-sm-4 col-sm-offset-4">
