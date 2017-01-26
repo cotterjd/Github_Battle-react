cac541950aea93abda7afbbffb56d0a01b55397a
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-26 15:08:33 -0600

    formatting

--- app/components/UserDetails.js
+++ app/components/UserDetails.js
@@ -1,26 +1,26 @@
 const
-	React = require('react')
+  React = require('react')
 , PropTypes = React.PropTypes
 , UserDetails = function (user){
-		return (
-			<div>
-				{!!user.score && <li className="list-group-item"><h3>Score: {user.score}</h3></li>}
-				<li className="list-group-item"> <img src={user.info.avatar_url} className="img-rounded img-responsive"/></li>
-				{user.info.name && <li className="list-group-item">Name: {user.info.name}</li>}
-				<li className="list-group-item">Username: {user.info.login}</li>
-				{user.info.location && <li className="list-group-item">Location: {user.info.location}</li>}
-				{user.info.company && <li className="list-group-item">Company: {user.info.company}</li>}
-				<li className="list-group-item">Followers: {user.info.followers}</li>
-				<li className="list-group-item">Following: {user.info.following}</li>
-				<li className="list-group-item">Public Repos: {user.info.public_repos}</li>
-				{user.info.blog && <li className="list-group-item">Blog: <a href={user.info.blog}> {user.info.blog}</a></li>}
-			</div>
+	return (
+		<div>
+			{!!user.score && <li className="list-group-item"><h3>Score: {user.score}</h3></li>}
+			<li className="list-group-item"> <img src={user.info.avatar_url} className="img-rounded img-responsive"/></li>
+			{user.info.name && <li className="list-group-item">Name: {user.info.name}</li>}
+			<li className="list-group-item">Username: {user.info.login}</li>
+			{user.info.location && <li className="list-group-item">Location: {user.info.location}</li>}
+			{user.info.company && <li className="list-group-item">Company: {user.info.company}</li>}
+			<li className="list-group-item">Followers: {user.info.followers}</li>
+			<li className="list-group-item">Following: {user.info.following}</li>
+			<li className="list-group-item">Public Repos: {user.info.public_repos}</li>
+			{user.info.blog && <li className="list-group-item">Blog: <a href={user.info.blog}> {user.info.blog}</a></li>}
+		</div>
 	  )
   }
-, nil = null;
+;
 
 UserDetails.propTypes = {
-	score: PropTypes.number,
+  score: PropTypes.number,
   info: PropTypes.shape({
   	avatar_url: PropTypes.string.isRequired,
   	blog: PropTypes.string,
@@ -31,7 +31,7 @@ UserDetails.propTypes = {
 		login: PropTypes.string.isRequired,
 		name: PropTypes.string,
 		public_repos: PropTypes.number.isRequired,
-	})
+  })
 }
 
 module.exports = UserDetails;
