88e3e6ab45331f9c87e73b1f26de23abb2ebafe5
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-26 15:07:27 -0600

    formatting

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,5 @@
 const
-	React = require('react')
+  React = require('react')
 , PropTypes = React.PropTypes
 , MainContainer = require('./MainContainer')
 , styles = require('../styles')
@@ -10,32 +10,32 @@ const
 , Loading = require('./Loading')
 
 , StartOver = function () {
+ 	return (
+		<div className='col-sm-12' style={styles.space}>
+			<Link to='playerOne'>
+				<button type='button' className='btn btn-lg btn-danger'>Start Over</button>
+			</Link>
+		</div>
+		)
+  }
+, Results = function(props){
+	if (props.isLoading === true) {
 		return (
-			<div className='col-sm-12' style={styles.space}>
-				<Link to='playerOne'>
-					<button type='button' className='btn btn-lg btn-danger'>Start Over</button>
-				</Link>
-			</div>
+			<Loading text="One Moment" speed={100}/>
 		)
 	}
-, Results = function(props){
-		if (props.isLoading === true) {
-			return (
-				<Loading text="One Moment" speed={100}/>
-			)
-		}
 
-		if (props.scores[0] === props.scores[1]) {
-			return (
-				<MainContainer>
-					<h1>It's a tie!</h1>
-					<StartOver />
-				</MainContainer>
+	if (props.scores[0] === props.scores[1]) {
+		return (
+			<MainContainer>
+				<h1>It's a tie!</h1>
+				<StartOver />
+			</MainContainer>
 			)
-		}
-		const
-			winningIndex = props.scores[0] > props.scores[1] ? 0 : 1,
-			losingIndex = winningIndex === 0 ? 1 : 0;
+	}
+	const
+		winningIndex = props.scores[0] > props.scores[1] ? 0 : 1,
+		losingIndex = winningIndex === 0 ? 1 : 0;
 		return (
 			<MainContainer>
 				<h1>Results</h1>
@@ -48,11 +48,10 @@ const
 					</UserDetailsWrapper>
 				</div>
 				<StartOver />
-			</MainContainer>
-			
+			</MainContainer>			
 		)
-	}
-, nil = null;
+  }
+;
 
 Results.protoTypes = {
 	isLoading: PropTypes.bool.isRequired,
