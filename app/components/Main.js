2d4920515915675022add0fe4fe5e5917de97c5a
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-26 14:54:30 -0600

    formatting

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,15 +4,12 @@ const
 , Main = React.createClass({
     render: function () {
       return (
-				<div className='main-container'>
-	  			{this.props.children}
-				</div>
+	<div className='main-container'>
+		{this.props.children}
+	</div>
       )
     }
   })
-
-, nil = null;
+;
 
 module.exports = Main;
-
-
