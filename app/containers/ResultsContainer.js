76d77c42d44b1e62bf4f5d3e5c8484e2c1c3d969
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-26 14:48:15 -0600

    Update ResultsContainer.js

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -1,34 +1,33 @@
 const
-	React = require('react')
+  React = require('react')
 , Results = require('../components/Results')
 , githubHelpers = require('../utils/githubHelpers')
 
 , ResultsContainer = React.createClass({
-		getInitialState: function () {
-			return {
-				isLoading: true,
-				scores: []
-			}
-		},
-		componentDidMount: function() {
-			githubHelpers.battle(this.props.location.state.playersInfo)
-				.then(function (scores) {
-					this.setState({
-						scores: scores,
-						isLoading: false
-					})
-				}.bind(this))
-		},
-		render: function () {
-			return (
-				<Results 
-					isLoading={this.state.isLoading} 
-					playersInfo={this.props.location.state.playersInfo}
-					scores={this.state.scores}/>
-			);
+	getInitialState: function () {
+		return {
+			isLoading: true,
+			scores: []
 		}
-	})
-
-, nil = null;
+	},
+	componentDidMount: function() {
+		githubHelpers.battle(this.props.location.state.playersInfo)
+			.then(function (scores) {
+				this.setState({
+					scores: scores,
+					isLoading: false
+				})
+			}.bind(this))
+	},
+	render: function () {
+		return (
+			<Results 
+				isLoading={this.state.isLoading} 
+				playersInfo={this.props.location.state.playersInfo}
+				scores={this.state.scores}/>
+		);
+	}
+})
+;
 
 module.exports = ResultsContainer;
