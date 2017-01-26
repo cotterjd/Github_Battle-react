c166442c654cf668b6fca8042feafd8c111004a8
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-26 15:09:08 -0600

    formatting

--- app/components/UserDetailsWrapper.js
+++ app/components/UserDetailsWrapper.js
@@ -1,16 +1,15 @@
 const
-	React = require('react')
+  React = require('react')
 , PropTypes = React.PropTypes
 
 , UserDetailsWrapper = function (props) {
-		return (
-			<div className='col-sm-6'>
-				<p className='lead'>{props.header}</p>
-				{props.children}
-			</div>
+	return (
+		<div className='col-sm-6'>
+			<p className='lead'>{props.header}</p>
+			{props.children}
+		</div>
 		)
-	}
-
-, nil = null;
+  }
+;
 
 module.exports = UserDetailsWrapper;
